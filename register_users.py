4ff300c951452d252a195bf64f99a3507413ceeb
pranamya207 <79407414+pranamya207@users.noreply.github.com>
2022-11-17 09:54:42 +0530

    Update register_users.py

--- register_users.py
+++ register_users.py
@@ -1,5 +1,5 @@
 import sqlite3
-def ad(Usid,password,email,usr):
+def ad(usid_,password,email,usr):
     if usr == 'User':
         conn = sqlite3.connect('User_database.db')
         c=conn.cursor()
@@ -8,11 +8,11 @@ def ad(Usid,password,email,usr):
                         user_password text,
                         email_id text)""")
         sql_query = """SELECT * FROM User where usid=?"""
-        c.execute(sql_query,(Usid,))
+        c.execute(sql_query,(usid_,))
         row=c.fetchall()
         if row != [] :
             return 0
-        c.execute("INSERT INTO User VALUES ('{}','{}','{}')".format(Usid,password,email))
+        c.execute("INSERT INTO User VALUES ('{}','{}','{}')".format(usid_,password,email))
         conn.commit()
         conn.close()
         return 1
